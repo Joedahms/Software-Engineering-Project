4d56c28efb32042b5bd354260426b86615f2a62a
Joedahms <dahms.joejoe@gmail.com>
2024-09-05 14:05:37 -0400

    Added comment to output

--- src/output.ts
+++ src/output.ts
@@ -1,3 +1,6 @@
+// Function for outputting to stdout. Pass the string you want to the function and
+// it will print it to stdout
+
 const readline = require('node:readline');
 
 export function writeOutput(input: string) {
