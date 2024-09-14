9c5c4d0bfbfc61b2d076d4a9b866927e50f75e35
Logan Pelkey <lepelkey@iu.edu>
2024-09-14 14:51:42 -0400

    Squashed commit of the following:
    commit 1732d01fa7345b9ed2224d557ecc8a7259cf1af0
    Merge: 4d6e3a2 2285d33
    Author: lpelkey23 <lpelkey23@yahoo.com>
    Date:   Sat Sep 14 14:37:40 2024 -0400
    
        Merge pull request #53 from Joedahms/ndjson-output
    
        Ndjson output
    
    commit 2285d33ab087bf5a70e3702618bbcfb55149639a
    Author: Joedahms <dahms.joejoe@gmail.com>
    Date:   Sat Sep 14 14:18:14 2024 -0400
    
        ./run URL_FILE now passes syntax checker
    
    commit 3d19c153fd33545d82e10647d12936e4f75e0e13
    Author: Joedahms <dahms.joejoe@gmail.com>
    Date:   Fri Sep 13 16:38:07 2024 -0400
    
        can output ndjson of all metrics. need to update documentation as I added new classes
    
    commit 0162b5f8d33dfd09d6b4f46ba6ba024b79259638
    Author: Joedahms <dahms.joejoe@gmail.com>
    Date:   Fri Sep 13 12:28:50 2024 -0400
    
        Integrated my ndjson testing into main.ts
    
    commit 0fe7a9660157709c8efc6ad95d860aea66764ca1
    Merge: 7a5dc5c 4d6e3a2
    Author: Joedahms <dahms.joejoe@gmail.com>
    Date:   Fri Sep 13 12:20:48 2024 -0400
    
        Resolved merge conflicts with main after pull
    
    commit 7a5dc5cec4b3f4bcbe32be0a33ba302a911a83f5
    Author: Joedahms <dahms.joejoe@gmail.com>
    Date:   Thu Sep 12 19:30:08 2024 -0400
    
        Started playing around with NDJSON and outputting it to stdout

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,55 @@
-import { Repository, UrlFileParser } from './urlFileParser.js';
+import { RepositoryUrlData, UrlFileParser } from './urlFileParser.js'; // interface, class
+import { Repository } from './repository.js'  // class
+import { writeOutput } from './output.js'     // function
 
 export class Main {
   readonly urlFileParser: UrlFileParser;
 
   constructor() {
     this.urlFileParser = new UrlFileParser();
-    this.GrabRepoNames(); // Call the async init method
   }
 
-  async GrabRepoNames() {
-    // Async function to handle async calls
-    var testRepo: Repository[] = [];
-    testRepo = await this.urlFileParser.npmRepos();
-    console.log(testRepo);
-    testRepo = this.urlFileParser.githubRepos();
-    console.log(testRepo);
+  // Get all the repo's owners and names from the url file
+  async parseUrlFile(): Promise<RepositoryUrlData[]> {
+    var repositoryUrlData: RepositoryUrlData[] = [];
+
+    repositoryUrlData = await this.urlFileParser.npmRepos();
+    repositoryUrlData = repositoryUrlData.concat(this.urlFileParser.githubRepos());
+
+    return Promise.resolve(repositoryUrlData); 
   }
 }
 
-function main(): void {
-  new Main();
+// New main object
+var main = new Main();
+
+// Get repo owners and names
+var urlData: RepositoryUrlData[] = [];
+urlData = await main.parseUrlFile();
+
+// Array of repository objects
+var repositories: Repository[] = [];
+
+// Set the correct url, owner, and name for each repository
+var urlDataIndex: number;
+for (urlDataIndex = 0; urlDataIndex < urlData.length; urlDataIndex++) {
+  var newRepository = new Repository( urlData[urlDataIndex].url, 
+                                      urlData[urlDataIndex].owner, 
+                                      urlData[urlDataIndex].name
+                                    );
+  repositories.push(newRepository);
 }
 
-main();
+// Print out metric calculation results in NDJSON
+// As of now, there aren't any calculations being done, defaults are printed
+var repositoryIndex: number;
+var output: string = "";
+for (repositoryIndex = 0; repositoryIndex < repositories.length; repositoryIndex++) {
+  output = output.concat(repositories[repositoryIndex].jsonMetrics());
+}
+
+//writeOutput(output);  // Cant figure out how to get the terminal caret to go away
+console.log(output);
+
+// Exit 
+process.exit(0);
